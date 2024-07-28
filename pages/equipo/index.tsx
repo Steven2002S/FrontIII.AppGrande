0df64ca54b85448d66bb4be0b73df05675109e17
Steven2002S <sjvargas2@espe.edu.ec>
2024-07-28 09:09:56 -0500

    Docente

--- pages/equipo/index.tsx
+++ pages/equipo/index.tsx
@@ -64,7 +64,7 @@ const EquipoPage: FC<PropsWithChildren<{}>> = ({ }) => {
               </div>
               <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
                 <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://ejemplo.com/nuevo-docente.jpg" />
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://res.cloudinary.com/di090ggjn/image/upload/v1722175666/unfleyel4ahsitcxza5n.jpg" />
                   <div className="flex-grow">
                     <h2 className="text-gray-900 title-font font-medium">Atal Kumar</h2>
                     <p className="text-gray-500">Docente de apoyo</p>
